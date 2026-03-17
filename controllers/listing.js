a88f5f14d076df56121eb140f20f94a12b020837
Shivam Kaushik <sk9548265968@gmail.com>
2026-03-17 12:47:10 -0700

    implement image upload feature using local file in new listing form

---
+++ controllers/listing.js
@@ -0,0 +1,70 @@
+const Listing = require("../models/listing");
+const Async = require("../utils/wrapAsync");
+
+module.exports.index = async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index", { allListings });
+
+};
+
+module.exports.renderNewForm = async (req, res) => {
+    res.render("listings/new");
+};
+
+module.exports.showListing = async (req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id)
+    .populate({path: "reviews", 
+        populate: {
+            path: "author"
+        },
+    })
+    .populate("owner");
+    if(!listing) {
+        req.flash("error", "Cannot find the listing!");
+        return res.redirect("/listings");
+    }
+    console.log(listing);
+    res.render("listings/show.ejs", {listing});
+};
+
+module.exports.createListing = async (req,res,next) => {
+        const newListing = new Listing(req.body.listing);
+        newListing.owner = req.user._id;
+        // If an image was uploaded via multer/cloudinary, store its URL.
+        if (req.file && req.file.path) {
+            newListing.image = req.file.path;
+        }
+    
+        await newListing.save();
+        req.flash("success", "Successfully created a new listing!");
+        res.redirect("/listings");
+    
+};
+
+module.exports.renderEditForm = async (req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    if(!listing) {
+        req.flash("error", "Listing does not exist!");
+        return res.redirect("/listings");
+    }
+    req.flash("success", "Successfully fetched the listing!");
+    res.render("listings/edit.ejs", {listing});
+};
+
+module.exports.updateListing = async (req,res) => {
+    
+    let {id} = req.params;
+    const listing = await Listing.findByIdAndUpdate(id, req.body.listing);
+    req.flash("success", "Successfully updated the listing!");
+    res.redirect(`/listings/${id}`);
+};
+
+module.exports.destroyListing = async (req,res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    req.flash("success", "Successfully deleted the listing!");
+    res.redirect("/listings");
+};
